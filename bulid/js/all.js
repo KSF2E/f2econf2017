1ae9b3127d5addefc13c313f18f3fa62ee183239
unaxie <una416@gmail.com>
2017-03-25 21:08:30 +0800

    update #8 and add place info

--- bulid/js/all.js
+++ bulid/js/all.js
@@ -3,4 +3,9 @@ $(function() {
         $(this).children('.fa-angle-up').toggleClass('fa-rotate-180');
         $(this).parent().next().find('.issueTopicIntro').stop().slideToggle();
     });
+
+    $('.detailplace_tab>li').click(function(event) {
+    	$(this).toggleClass('is-active').siblings().toggleClass('is-active');
+    	$('.detailplace_info').children().toggleClass('hide');
+    });
 });
